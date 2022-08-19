4a890414ff3bfa2ec698f4494715a092a9a7bc8a
RozoMartinez <lilirozomartinez@gmail.com>
2022-08-19 17:58:03 -0500

    Colocamos comandos de github

--- ejemplo.py
+++ ejemplo.py
@@ -1,1 +1,9 @@
 a = "Hola mundo"
+
+
+#Comandos de github 
+#git status ----> muestra cuáles son los archivos que necesitan commit
+#git add ejemplo.py -----> agregar al repositorio el archivo ejemplo.py
+#git commit -m "Creamos variable "hola mundo"---> comentario de lo que hice en el código. 
+#git push origin main ------> sube lo que hagamos al repositorio
+
